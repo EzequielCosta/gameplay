3aee3fc2d05d6c7808f8614ead8972b3c574f1d8
EzequielCosta <ezequielteixeiracosta321@gmail.com>
2021-10-01 02:43:43 -0300

    Segunda aula em andamento

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,15 +1,41 @@
 import React, { useState } from "react";
-import { View , Text, Image } from "react-native";
-import { style } from "./style";
+import { View , Text, Image, StatusBar } from "react-native";
+import { styles } from "./style";
 import IllustrationImg from '../../assets/illustration.png';
+import { ButtonIcon } from "../../components/ButtonIcon";
+
 
 export function SignIn() { 
 
-    const [text, setText] = useState("Ezequiel")
+    
 
     return (
-        <View style = {style.container}>
-            <Image source = {IllustrationImg} />
+        <View style = {styles.container}>
+            
+            <Image
+                source = {IllustrationImg}
+                style = {styles.image}
+                resizeMode='stretch'
+            />
+            <View style={styles.content}>
+                <Text style={styles.title}>
+                    Conecte-se {'\n'}
+                    e organize suas{'\n'}
+                    jogatinas
+                </Text>
+                <Text style={styles.subtitle}>
+                    Crie grupos para jogar seus games {'\n'}
+                    favoritos com seus amigos
+                </Text>
+                <ButtonIcon 
+                    title='Entrar com Discord'
+                    activeOpacity = {0.7}
+                />
+            </View>
+
+            
+
         </View>
+        
     )
 }
